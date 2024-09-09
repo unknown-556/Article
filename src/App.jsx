985cb8bdda8d1861e9be849f32e69e28595a877c
unknown-556 <obasio1115@gmail.com>
2024-09-09 14:05:27 +0100

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+
+import './index.css'
+
+function App() {
+  return (
+    <div className="flex items-center justify-center h-screen bg-gray-800">
+      <h1 className="text-4xl font-bold text-white">Hello, Tailwind CSS!</h1>
+    </div>
+  );
+}
+
+export default App;
